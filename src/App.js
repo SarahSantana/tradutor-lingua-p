59b1dd21eb314643663710097f165fce66da1675
SARAH SANTANA PEREIRA <sarah.pereira-ext@viavarejo.com.br>
2020-07-20 22:18:21 -0300

    adicionando separador de silabas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+let consoantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z'];
+let vogais = ['a', 'e', 'i', 'o', 'u'];
+
+function isConsoante(letra) {
+  return !!(consoantes.find((consoante) => consoante === letra));
+}
+
+function isVogal(letra) {
+  return !!(vogais.find((vogal) => vogal === letra));
+}
+
 function App() {
+
+  let palavra = 'Sarah Santana Pereira';
+  palavra = palavra.toLowerCase();
+  let palavras = palavra.split(' ');
+
+  let silabas = [];
+  let letras = [];
+
+  palavras.forEach((palavra, indexPalavra) => {
+    letras[indexPalavra] = palavra.split('');
+    console.log('letras[indexPalavra]', letras[indexPalavra]);
+    
+    letras[indexPalavra].forEach((letra, indexLetra) => {
+      if (this.isConsoante(letra)) {
+        if (this.isVogal(letras[indexPalavra][indexLetra + 1])) {
+          
+        }
+
+      } else if (this.isVogal(letra)) {
+        if (indexLetra === 0) {
+          silabas[indexPalavra] = 'pe'+letra;
+          return;
+        }
+      }
+    })
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {silabas[0]}
       </header>
     </div>
   );
