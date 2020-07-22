029baf4655aa73711cb612c98893256cfdb99caa
SARAH SANTANA PEREIRA <sarah.pereira-ext@viavarejo.com.br>
2020-07-21 22:11:26 -0300

    arrumando coisas feias

--- src/App.js
+++ src/App.js
@@ -1,52 +1,110 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-let consoantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z'];
-let vogais = ['a', 'e', 'i', 'o', 'u'];
+class App extends Component {
 
-function isConsoante(letra) {
-  return !!(consoantes.find((consoante) => consoante === letra));
-}
+  constructor(props) {
+    super(props);
 
-function isVogal(letra) {
-  return !!(vogais.find((vogal) => vogal === letra));
-}
+    this.state = {
+      consoantes: [],
+      vogais: [],
+      frase: String,
+      palavras: [],
+      silabas: [],
+      letras: []
+    };
 
-function App() {
+    // inicia valores do state
+    this.init();
+  }
 
-  let palavra = 'Sarah Santana Pereira';
-  palavra = palavra.toLowerCase();
-  let palavras = palavra.split(' ');
+  init() {
+    // define letras
+    this.state.consoantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z'];
+    this.state.vogais = ['a', 'e', 'i', 'o', 'u'];
+    // frase que o usuario digitara
+    this.state.frase = 'Sarah Santana Pereira';
+    // inicia array com as palavras da frase
+    this.state.palavras = [];
+    // inicia array de silabas (guardara silabas de cada palavra)
+    this.state.silabas = [];
+    // inicia array de letras (guardara array de letras de cada palavra)
+    this.state.letras = [];
 
-  let silabas = [];
-  let letras = [];
+    // separa as silabas das palavras da frase
+    this.encontraSilabas();
+  }
 
-  palavras.forEach((palavra, indexPalavra) => {
-    letras[indexPalavra] = palavra.split('');
-    console.log('letras[indexPalavra]', letras[indexPalavra]);
-    
-    letras[indexPalavra].forEach((letra, indexLetra) => {
-      if (this.isConsoante(letra)) {
-        if (this.isVogal(letras[indexPalavra][indexLetra + 1])) {
-          
-        }
+  encontraSilabas() {
+    this.state.palavras = this.separaPalavras();
 
-      } else if (this.isVogal(letra)) {
-        if (indexLetra === 0) {
-          silabas[indexPalavra] = 'pe'+letra;
-          return;
+    this.state.palavras.forEach((palavra, indexPalavra) => {
+      // separa a palavra em letras
+      this.state.letras[indexPalavra] = palavra.split('');
+
+      const primeiraLetraSilaba = this.state.letras[indexPalavra][0];
+      const primeiraIsConsoante = this.isConsoante(primeiraLetraSilaba);
+      const primeiraIsVogal = this.isVogal(primeiraLetraSilaba);
+      // sa-rah
+
+      // percorre cada letra da palavra a partir da segunda letra
+      for(let indexLetra = 1; indexLetra < this.state.letras[indexPalavra].length; indexLetra++) {
+        // guarda letra correspondente ao indice dentro da palavra
+        let letra = this.state.letras[indexPalavra][indexLetra];
+
+        // verifica se a letra e consoante
+        let letraIsConsoante = this.isConsoante(letra);
+        // verifica se a letra e vogal
+        let letraIsVogal = this.isVogal(letra);
+
+        if (letraIsConsoante && primeiraIsConsoante) {
+          // as duas formam uma silaba
+          // troca a primeiraLetraSilaba (adicionar um for nas letras da palavra cada
+          // vez que formar uma silaba passa para a proxima letra depois da ultima letra
+          // da silaba que se formou)
+        } else if (letraIsVogal && primeiraIsConsoante) {
+          // as duas podem ou nao formar uma silaba
+          // guarda essa letra numa variavel (letraPendente)
+          // para testar se a proxima letra é uma consoante ou vogal
+          // se a proxima for vogal, entao essas duas formam uma silaba 
+          // se a proxima for uma consoante,
+          // testar se ainda tem letras nessa palavra, se nao tiver mais letras entao essas duas 
+          // formam uma silaba 
+
+          // testar se essa vogal e a primeira letra da palavra se for entao 
+          // deixa pendente essa letra e testa se a proxima e uma vogal tbm
+          // se a proxima for uma vogal tbm entao essa primeira letra vogal
+          // forma uma silaba sozinha
         }
       }
-    })
-  });
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        {silabas[0]}
-      </header>
-    </div>
-  );
+    });
+  }
+
+  separaPalavras() {
+    this.state.frase = this.state.frase.toLowerCase();
+    return this.state.frase.split(' ');
+  }
+
+  isConsoante(letra) {
+    return !!(this.state.consoantes.find((consoante) => consoante === letra));
+  }
+
+  isVogal(letra) {
+    return !!(this.state.vogais.find((vogal) => vogal === letra));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React é legal</h1>
+        {this.state.palavras.map(palavra => 
+          <p>{palavra}</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
